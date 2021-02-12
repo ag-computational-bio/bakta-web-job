5dc11bdd3c418e2eba6370ad4c7443c1867db7ba
Marius <mariusdieckmann90@gmail.com>
2021-02-12 14:47:57 +0100

    added update handler to run skript

--- run.sh
+++ run.sh
@@ -9,3 +9,4 @@ eval "/bin/DataStager $DownloaderEnvConfig"
 eval "/entrypoint.sh "$BaktaEnvConfig" -o /output --tmp-dir /cache -t 4"
 tar -czvf results.tar.gz /output
 eval "/bin/DataStager $UploaderEnvConfig"
+eval "/bin/DataStager update"
