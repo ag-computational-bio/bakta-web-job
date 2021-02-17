27030202df1960d4c27d9a423a26cd48d4857320
Marius <mariusdieckmann90@gmail.com>
2021-02-18 00:31:20 +0100

    removed tar line in script

--- run.sh
+++ run.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
-bash -euo pipefail 
+set -e
 
 mkdir /data
 mkdir /output
 
 eval "/bin/DataStager $DownloaderEnvConfig"
 eval "/entrypoint.sh "$BaktaEnvConfig" -o /output --tmp-dir /cache -t 4"
-tar -czvf results.tar.gz /output
 eval "/bin/DataStager $UploaderEnvConfig"
 eval "/bin/DataStager update"
