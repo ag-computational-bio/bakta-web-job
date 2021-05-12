95481939a7d3dad67fd7973231d63b0d00d986db
Marius <mariusdieckmann90@gmail.com>
2021-05-12 14:05:41 +0200

    new shell logic

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+set -eou pipefail
+
+function cleanup {
+    /bin/DataStager update
+}
+trap cleanup EXIT
 
 mkdir /data
 mkdir /output
 
-eval "/bin/DataStager $DownloaderEnvConfig" && eval "/entrypoint.sh "$BaktaEnvConfig"" && eval "/bin/DataStager $UploaderEnvConfig"
-eval "/bin/DataStager update"
+/bin/DataStager "$DownloaderEnvConfig"
+/entrypoint.sh "$BaktaEnvConfig"
+/bin/DataStager "$UploaderEnvConfig"
+/bin/DataStager update
+echo "after possible failure"
