243d8e7cdc8ccc4717261a72d221de92d9c4e422
Marius <mariusdieckmann90@gmail.com>
2021-05-12 08:50:13 +0200

    using logical && for command execution

--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-set -e
 
 mkdir /data
 mkdir /output
 
-eval "/bin/DataStager $DownloaderEnvConfig"
-eval "/entrypoint.sh "$BaktaEnvConfig""
-eval "/bin/DataStager $UploaderEnvConfig"
+eval "/bin/DataStager $DownloaderEnvConfig" && eval "/entrypoint.sh "$BaktaEnvConfig"" && eval "/bin/DataStager $UploaderEnvConfig"
 eval "/bin/DataStager update"
