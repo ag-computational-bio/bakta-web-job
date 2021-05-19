69aa30aa4f791f8d43486a81e3f689aa81554c91
Marius <mariusdieckmann90@gmail.com>
2021-05-19 17:30:54 +0200

    new structure without update functions

--- run.sh
+++ run.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 set -eou pipefail
 
-function cleanup {
-    /bin/DataStager update
-}
-trap cleanup EXIT
-
 mkdir /data
 mkdir /output
 
 /bin/DataStager $DownloaderEnvConfig
-
 sleep 3600
-
 /entrypoint.sh $BaktaEnvConfig
 /bin/DataStager $UploaderEnvConfig
-/bin/DataStager update
-echo "after possible failure"
