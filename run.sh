70d70d118abd7e60dcd00f1f44c68a130638b247
Marius <mariusdieckmann90@gmail.com>
2021-05-19 18:08:45 +0200

    rolled back to old structure with updates

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -eou pipefail
 
+function cleanup {
+    /bin/DataStager update
+}
+trap cleanup EXIT
+
 mkdir /data
 mkdir /output
 
 /bin/DataStager $DownloaderEnvConfig
 /entrypoint.sh $BaktaEnvConfig
 /bin/DataStager $UploaderEnvConfig
+/bin/DataStager update
+echo "after possible failure"
